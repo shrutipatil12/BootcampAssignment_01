f32d96670f999f4d0e13f2477fe5beefe4d4070b
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-09-02 01:25:27 +0530

    [Shruti] Add. calculate total amount paid by one friend on two friends

--- src/test/java/TestFriend.java
+++ src/test/java/TestFriend.java
@@ -29,5 +29,13 @@ public class TestFriend {
         AmountToPay friend = new Friends(name,threeHundredMoneyPaid,zeroMoneyToPay);
         Assertions.assertEquals(300,friend.payAmount());
     }
+        @Test
+    public void givenSpentZeroMoneyByBunnyForTwoFriends_WhenCalculate_ThenShouldReturnZero(){
+        String name="Bunny";
+        double zeroMoneyPaid = 0.0;
+        double zeroMoneyToPay=0.0;
+        AmountToPay friend = new Friends(name,zeroMoneyPaid,zeroMoneyToPay);
+        Assertions.assertEquals(0.0,friend.payAmount());
+    }
 
 }
